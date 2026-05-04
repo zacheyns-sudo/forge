9d2f7c10b7bb356a541b313d786ccfcc6bebf9c9
Zac Heyns <zacheyns@macbookpro.home>
2026-05-04 10:23:50 +0200

    fix: persist error handling, request persistent storage, bump SW cache v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'forge-v1';
+const CACHE = 'forge-v2';
 const SHELL = ['/', '/index.html', '/manifest.json', '/icon.svg', '/icon-maskable.svg'];
 
 self.addEventListener('install', e => {
